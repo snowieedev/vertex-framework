21e07e0d6a9279ecc56eafd1322de9e62f32a596
snowieedev <snowieee.dev@gmail.com>
2026-06-21 22:08:08 +0530

    chore: bump versions to 0.1.2

--- release.ps1
+++ release.ps1
@@ -1,6 +1,5 @@
 pnpm -r version patch
-pnpm run build
 git add .
-git commit -m "fix: add dev scripts and upgrade ESLint to v9"
+git commit -m "chore: bump versions to 0.1.2"
 git push
 pnpm publish -r --access public --no-git-checks
